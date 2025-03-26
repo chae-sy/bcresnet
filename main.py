5fbebc6355677ec815bfa60371b0687429631723
chae-sy <sendemailtocsy@gmail.com>
2025-03-26 13:35:40 +0900

    commit

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Trainer:
         Trains the model and presents the train/test progress.
         """
         # train hyperparameters
-        total_epoch =20
+        total_epoch =200
         warmup_epoch = 5
         init_lr = 1e-1
         lr_lower_limit = 0
