342f4e1f50dcde09f63fd18200d100598c89d46f
chae-sy <synnchae@gmail.com>
2025-03-31 19:10:55 +0900

    remove quantization

--- model.py
+++ model.py
@@ -3,11 +3,9 @@
 
 import torch.nn.functional as F
 from torch import nn
-import torch
-import torch.nn.init as init
 
 from subspectralnorm import SubSpectralNorm
-from module import ActFn, Conv2d, Linear
+
 
 class ConvBNReLU(nn.Module):
     def __init__(
@@ -15,13 +13,12 @@ class ConvBNReLU(nn.Module):
         in_plane,
         out_plane,
         idx,
-        bitwidth,
         kernel_size=3,
         stride=1,
         groups=1,
         use_dilation=False,
         activation=True,
-        #swish=False,
+        swish=False,
         BN=True,
         ssn=False,
     ):
@@ -36,8 +33,7 @@ class ConvBNReLU(nn.Module):
             return padding_len, rate
 
         self.idx = idx
-        self.bitwidth = bitwidth
-        self.act=False # default= False
+
         # padding and dilation rate
         if isinstance(kernel_size, (list, tuple)):
             padding = []
@@ -52,50 +48,38 @@ class ConvBNReLU(nn.Module):
         # convbnrelu block
         layers = []
         layers.append(
-            Conv2d(in_plane, out_plane, kernel_size, stride, padding, rate, groups, bias=False, bitwidth=self.bitwidth)
+            nn.Conv2d(in_plane, out_plane, kernel_size, stride, padding, rate, groups, bias=False)
         )
         if ssn:
             layers.append(SubSpectralNorm(out_plane, 5))
         elif BN:
             layers.append(nn.BatchNorm2d(out_plane))
-        # if swish:
-        #     layers.append(nn.SiLU(True))
-        # elif activation:
-        #     layers.append(nn.ReLU(True))
-        if activation:
-            self.act = True
+        if swish:
+            layers.append(nn.SiLU(True))
+        elif activation:
+            layers.append(nn.ReLU(True))
         self.block = nn.Sequential(*layers)
-        self.apply(_weights_init)
 
     def forward(self, x):
-        #print(f"ConvBNReLU Input: {x.shape}")  # Debug print
-        x = self.block(x)
-        if self.act:
-            self.ActFn = ActFn.apply
-            self.alpha=nn.Parameter(torch.tensor(10.0))
-            x = self.ActFn(x, self.alpha, self.bitwidth)
-        #print(f"ConvBNReLU Output: {x.shape}")  # Debug print
-        return x
-        #return self.block(x)
+        return self.block(x)
 
 
 class BCResBlock(nn.Module):
-    def __init__(self, in_plane, out_plane, idx, stride, bitwidth):
+    def __init__(self, in_plane, out_plane, idx, stride):
         super().__init__()
         self.transition_block = in_plane != out_plane
         kernel_size = (3, 3)
-        self.bitwidth=bitwidth
+
         # 2D part (f2)
         layers = []
         if self.transition_block:
-            layers.append(ConvBNReLU(in_plane, out_plane, idx, self.bitwidth, 1, 1,))
+            layers.append(ConvBNReLU(in_plane, out_plane, idx, 1, 1))
             in_plane = out_plane
         layers.append(
             ConvBNReLU(
                 in_plane,
                 out_plane,
                 idx,
-                self.bitwidth,
                 (kernel_size[0], 1),
                 (stride[0], 1),
                 groups=in_plane,
@@ -112,20 +96,17 @@ class BCResBlock(nn.Module):
                 out_plane,
                 out_plane,
                 idx,
-                self.bitwidth,
                 (1, kernel_size[1]),
                 (1, stride[1]),
                 groups=out_plane,
-                #swish=True,
+                swish=True,
                 use_dilation=True,
             ),
-            Conv2d(out_plane, out_plane, 1, bias=False, bitwidth = self.bitwidth),
+            nn.Conv2d(out_plane, out_plane, 1, bias=False),
             nn.Dropout2d(0.1),
         )
-        self.apply(_weights_init)
 
     def forward(self, x):
-        #print(f"BCResBlock Input: {x.shape}")  # Debug print
         # 2D part
         shortcut = x
         x = self.f2(x)
@@ -137,28 +118,21 @@ class BCResBlock(nn.Module):
         x = x + aux_2d_res
         if not self.transition_block:
             x = x + shortcut
-        
-        self.ActFn = ActFn.apply
-        self.alpha = nn.Parameter(torch.tensor(10.0))
-        x = self.ActFn(x, self.alpha, self.bitwidth)
-        #print(f"BCResBlock Output: {x.shape}")  # Debug print
+        x = F.relu(x, True)
         return x
-    
-        # x = F.relu(x, True)
-        # return x
 
 
-def BCBlockStage(num_layers, last_channel, cur_channel, idx, use_stride, bitwidth):
+def BCBlockStage(num_layers, last_channel, cur_channel, idx, use_stride):
     stage = nn.ModuleList()
     channels = [last_channel] + [cur_channel] * num_layers
     for i in range(num_layers):
         stride = (2, 1) if use_stride and i == 0 else (1, 1)
-        stage.append(BCResBlock(in_plane=channels[i], out_plane=channels[i + 1], idx=idx, stride=stride, bitwidth=bitwidth))
+        stage.append(BCResBlock(channels[i], channels[i + 1], idx, stride))
     return stage
 
 
 class BCResNets(nn.Module):
-    def __init__(self, base_c, bitwidth, num_classes=12):
+    def __init__(self, base_c, num_classes=12):
         super().__init__()
         self.num_classes = num_classes
         self.n = [2, 2, 4, 4]  # identical modules repeated n times
@@ -171,68 +145,38 @@ class BCResNets(nn.Module):
             base_c * 4,
         ]  # num channels
         self.s = [1, 2]  # stage using stride
-        self.bitwidth=bitwidth
         self._build_network()
 
-    def _build_network(self,): 
+    def _build_network(self):
         # Head: (Conv-BN-ReLU)
-        #def __init__(self, in_places, out_planes, kernel_size, stride=1, padding = 0, groups=1, dilation=1, bias = False, bitwidth = 8):
-            
         self.cnn_head = nn.Sequential(
-            Conv2d(1, self.c[0], 5, (2, 1), 2, bias=False, bitwidth=self.bitwidth),
+            nn.Conv2d(1, self.c[0], 5, (2, 1), 2, bias=False),
             nn.BatchNorm2d(self.c[0]),
-            #nn.ReLU(True),     
+            nn.ReLU(True),
         )
         # Body: BC-ResBlocks
         self.BCBlocks = nn.ModuleList([])
         for idx, n in enumerate(self.n):
             use_stride = idx in self.s
-            self.BCBlocks.append(BCBlockStage(n, self.c[idx], self.c[idx + 1], idx, use_stride, bitwidth=self.bitwidth))
+            self.BCBlocks.append(BCBlockStage(n, self.c[idx], self.c[idx + 1], idx, use_stride))
 
         # Classifier
         self.classifier = nn.Sequential(
-            Conv2d(self.c[-2], self.c[-2], (5, 5), bias=False, groups=self.c[-2], padding=(0, 4), bitwidth=self.bitwidth),
-            Conv2d(self.c[-2], self.c[-1], 1, bias=False, bitwidth=self.bitwidth),
-            nn.BatchNorm2d(self.c[-1]),)
-        
-        self.classifier2=nn.Sequential(
-            #nn.ReLU(True),
+            nn.Conv2d(
+                self.c[-2], self.c[-2], (5, 5), bias=False, groups=self.c[-2], padding=(0, 2)
+            ),
+            nn.Conv2d(self.c[-2], self.c[-1], 1, bias=False),
+            nn.BatchNorm2d(self.c[-1]),
+            nn.ReLU(True),
             nn.AdaptiveAvgPool2d((1, 1)),
-            Conv2d(self.c[-1], self.num_classes, 1, bitwidth=self.bitwidth),
+            nn.Conv2d(self.c[-1], self.num_classes, 1),
         )
-        self.apply(_weights_init)
 
     def forward(self, x):
-        self.ActFn = ActFn.apply
-        
-        #print(f"Model Input: {x.shape}")  # Debug print
-        self.cnn_head_alpha = nn.Parameter(torch.tensor(10.0))
         x = self.cnn_head(x)
-        x= self.ActFn(x, self.cnn_head_alpha, self.bitwidth)
-        #print(f"After CNN Head: {x.shape}")  # Debug print
         for i, num_modules in enumerate(self.n):
             for j in range(num_modules):
                 x = self.BCBlocks[i][j](x)
-                #print(f"After BCBlock {i}-{j}: {x.shape}")  # Debug print
-        #print(self.classifier[0].weight.data.shape)        
-        self.classifier_alpha = nn.Parameter(torch.tensor(10.0))        
         x = self.classifier(x)
-        x = self.ActFn(x, self.classifier_alpha, self.bitwidth)
-        #print(f"After Classifier: {x.shape}")  # Debug print
-        
-        x = self.classifier2(x)
-        #print(f"After Classifier2: {x.shape}")  # Debug print
-        
         x = x.view(-1, x.shape[1])
-        #print(f"Final Output: {x.shape}")  # Debug print
         return x
-    
-def _weights_init(m):
-    """
-    Initialize weights using Kaiming Normal Initialization
-    """
-    #classname = m.__class__.__name__
-    ##print(classname)
-    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d): 
-        init.kaiming_normal_(m.weight) # Kaiming Normal Initialization
-
