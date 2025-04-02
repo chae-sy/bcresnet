3e61377573a8b1570ca9bf141f68857c509519f4
chae-sy <sychaeee@g.skku.edu>
2025-04-02 13:41:42 +0000

    from container

--- main.py
+++ main.py
@@ -107,8 +107,8 @@ class Trainer:
         transform = transforms.Compose([Padding()])
         self.train_dataset = PKMTLDataset(SpeechCommandWithSpeaker(train_dir, self.ver, transform=transform))
         self.valid_dataset = PKMTLDataset(SpeechCommandWithSpeaker(valid_dir, self.ver, transform=transform))
-        self.train_loader = DataLoader(self.train_dataset, batch_size=1024, shuffle=True, num_workers=0)#4, pin_memory=True)
-        self.valid_loader = DataLoader(self.valid_dataset, batch_size=1024, shuffle=False, num_workers=0)#4, pin_memory=True)
+        self.train_loader = DataLoader(self.train_dataset, batch_size=1024, shuffle=True, num_workers=4, pin_memory=True)
+        self.valid_loader = DataLoader(self.valid_dataset, batch_size=1024, shuffle=False, num_workers=4, pin_memory=True)
 
         specaugment = self.tau >= 1.5
         freq_masking = {1: 0, 1.5: 1, 2: 3, 3: 5, 6: 7, 8: 7}
